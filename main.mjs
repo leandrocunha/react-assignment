5948870fcc7b40aa72beb267afc779487de43bbd
Leandro Cunha <leandroscunha@gmail.com>
2018-10-04 00:47:14 -0300

    feature: make a simple api call

--- main.mjs
+++ main.mjs
@@ -1,9 +1,11 @@
-import express from 'express';
 import axios from 'axios';
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
 const apiURL = 'http://api.football-data.org/v2';
 
+app.use(cors());
 app.get('/api/competitions', (req, res) => {
     axios.get(`${apiURL}/competitions`).then(r => res.json({ ...r.data }));
 });
--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
-);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+    }
+    componentDidMount() {
+        fetch('http://localhost:3000/api/competitions')
+            .then(body => body.json())
+            .then((res) => {
+                const netherlands = res.competitions.filter(competition => competition.area.name === 'Netherlands');
+                this.setState({ loading: false, competitions: netherlands });
+            });
+    }
+
+    render() {
+        const { competitions, loading } = this.state;
+        return (
+            <div className="App">
+                {loading ? (
+                    <p>loading...</p>
+                ) : (
+                    <select>
+                        {competitions.map(competition => (
+                            <option>{competition.name}</option>
+                        ))}
+                    </select>
+                )}
+            </div>
+        );
+    }
+}
 
 export default App;
