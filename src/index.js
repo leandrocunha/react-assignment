c8d06592ed2741c637281122b6afbb8af156dab2
Leandro Cunha <leandroscunha@gmail.com>
2018-10-03 23:22:26 -0300

    improvement: eslint extended endouble config

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(() => {},
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
 );
 
 registerServiceWorker();
