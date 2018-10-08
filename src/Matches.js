255b7688a63d27b0e5b3054194c0429a1804392e
Leandro Cunha <leandroscunha@gmail.com>
2018-10-08 01:09:26 -0300

    improvement(matches): layout

--- src/Matches.js
+++ src/Matches.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 import { connect } from 'react-redux';
@@ -24,14 +25,32 @@ const Matches = props => (
         {props.competition.matches &&
             !props.competition.matches.loading && (
                 <div className="Matches">
+                    <p className="Matches__Title">Matches</p>
                     <Slider {...settings}>
                         {props.competition.matches.list.map((round, index) => (
                             <div className="Matches__Round" key={round[0].id}>
                                 <p className="Matches__Round__Title">{`Round ${index + 1}`}</p>
-                                <ul>
+                                <ul className="Matches__Round__List">
                                     {round.map(match => (
-                                        <li key={match.id}>
-                                            {match.homeTeam.name} x {match.awayTeam.name}
+                                        <li className="Matches__Round__List__Item" key={match.id}>
+                                            <p className="Matches__Round__List__Item__Date">
+                                                {moment(match.utcDate).format('MM-DD-YYYY')}
+                                            </p>
+                                            <div className="Matches__Round__List__Item__Match">
+                                                <p className="Matches__Round__List__Item__Match__Home">
+                                                    <span className="Matches__Round__List__Item__Match__Home__Score">
+                                                        {match.score.fullTime.homeTeam}
+                                                    </span>
+                                                    {match.homeTeam.name}
+                                                </p>
+                                                <p className="Matches__Round__List__Item__Match__Versus">x</p>
+                                                <p className="Matches__Round__List__Item__Match__Away">
+                                                    <span className="Matches__Round__List__Item__Match__Away__Score">
+                                                        {match.score.fullTime.awayTeam}
+                                                    </span>
+                                                    {match.awayTeam.name}
+                                                </p>
+                                            </div>
                                         </li>
                                     ))}
                                 </ul>
